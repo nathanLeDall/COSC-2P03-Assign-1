a296f4f1771050acfaacbb39edcc804c3b4579a2
nathanLeDall <96305350+nathanLeDall@users.noreply.github.com>
2022-09-13 12:29:13 -0400

    commit

--- src/Assign1/Clinic.java
+++ src/Assign1/Clinic.java
@@ -1,4 +1,26 @@
 package Assign1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Clinic {
+    private Patient[] patients;
+    public void readData() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("patients.txt"));
+        //br.readLine();
+        while(true){
+            br.
+            String tmp = br.readLine();
+            if (tmp==null){
+                break;
+            }
+            System.out.println(tmp);
+        }
+    }
+
+    public static void main(String[] args) throws  IOException{
+        Clinic c =new Clinic();
+        c.readData();
+    }
 }
