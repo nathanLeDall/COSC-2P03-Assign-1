6c949b243aa4b222235931127d880d2dad5f1f0d
nathanLeDall <96305350+nathanLeDall@users.noreply.github.com>
2022-09-16 01:14:24 -0400

    commit

--- src/Assign1/Clinic.java
+++ src/Assign1/Clinic.java
@@ -6,46 +6,57 @@ import java.io.IOException;
 
 
 public class Clinic {
-    private final Patient[] patients = new Patient[15];
+    public final Patient[] patients = new Patient[15];
+    public final WaitQueue wq = new WaitQueue();
     public void readData() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("patients.txt"));
         String patientData = br.readLine();
         for (int i=0; i<15; i++) {
             patientData = br.readLine();
             String[] patientDataArray = patientData.split("\t",6);
-            patientDataArray[4] = patientDataArray[4].replace(":","");
+            patientDataArray[5] = patientDataArray[5].replace(":","");
             patients[i] = new Patient(patientDataArray);
             patients[i].setPriority(priorityCalculation(patients[i]));
-
             System.out.println(patients[i].toString());
         }
+    }
+
+    public void print(String[] tmp){
+        for (String tmp2: tmp){
+            System.out.print(tmp2);
+        }
+        System.out.println();
     }
 
     private int priorityCalculation(Patient patient){
         int priority=0;
         if (patient.getAge()>=60){priority++;}
-
-        if (patient.getOccupation().equals("Nurse") &&
-                patient.getOccupation().equals("Care Giver") &&
+        if (patient.getOccupation().equals("Nurse") ||
+                patient.getOccupation().equals("Care Giver") ||
                 patient.getOccupation().equals("Teacher"))
         {
             priority++;
         }
-        if (patient.getHealthCondition().equals(" Primary Immune Deficiency") &&
-                patient.getHealthCondition().equals("Cancer") &&
-                patient.getHealthCondition().equals("Diabetes") &&
-                patient.getHealthCondition().equals("Pregnant") &&
-                patient.getHealthCondition().equals("Asthma") &&
+        if (patient.getHealthCondition().equals(" Primary Immune Deficiency") ||
+                patient.getHealthCondition().equals("Cancer") ||
+                patient.getHealthCondition().equals("Diabetes") ||
+                patient.getHealthCondition().equals("Pregnant") ||
+                patient.getHealthCondition().equals("Asthma") ||
                 patient.getHealthCondition().equals("Cardiovascular Disease"))
         {
             priority++;
         }
-
         return priority;
     }
 
     public static void main(String[] args) throws  IOException{
-        Clinic c =new Clinic();
+        Clinic c = new Clinic();
         c.readData();
+        for (Patient p: c.patients){
+            c.wq.insert(p);
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println(c.wq);
     }
 }
--- src/Assign1/WaitQueue.java
+++ src/Assign1/WaitQueue.java
@@ -7,9 +7,9 @@ public class WaitQueue {
 
     /**
      *
-     * @param patients
+     *
      */
-    public WaitQueue(Patient[] patients){
+    public WaitQueue(){
         startOfQueue = null;
     }
 
@@ -19,7 +19,7 @@ public class WaitQueue {
      */
     public void insert(Patient patient){
         Node tmp = startOfQueue;
-        while(tmp!=null){
+        while(true){
             if (startOfQueue==null){
                 startOfQueue = new Node(patient);
                 return;
@@ -28,13 +28,35 @@ public class WaitQueue {
                 startOfQueue.setNext(new Node(patient));
                 return;
             }
+
             if (startOfQueue.getNext()==null && startOfQueue.getPatient().getPriority()<patient.getPriority()){
                 startOfQueue.setPrev(new Node(patient));
                 startOfQueue = startOfQueue.getPrev();
                 return;
             }
+
+            if (startOfQueue.getNext()==null && startOfQueue.getPatient().getPriority()==patient.getPriority()){
+                if (startOfQueue.getPatient().getTimeOfArrival()< patient.getTimeOfArrival()){
+                    startOfQueue.setNext(new Node(patient));
+                    return;
+                }
+                if (startOfQueue.getPatient().getTimeOfArrival()> patient.getTimeOfArrival()){
+                    Node tmp2 = new Node(patient,startOfQueue,null);
+                    startOfQueue.setPrev(tmp2);
+                    startOfQueue = tmp2;
+                    return;
+                }
+
+            }
+
             if (tmp.getNext()==null && tmp.getPatient().getPriority()> patient.getPriority()){
                 tmp.setNext(new Node(patient));
+                return;
+            }
+            if (tmp.getPrev()==null && tmp.getPatient().getPriority()< patient.getPriority()){
+                Node tmp3 = new Node(patient,tmp,null);
+                tmp.setPrev(tmp3);
+                startOfQueue = tmp3;
                 return;
             }
             if (tmp.getPatient().getPriority()< patient.getPriority()){
@@ -61,5 +83,22 @@ public class WaitQueue {
      */
     public Patient removeMax(){
         return new Patient();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder tmp = new StringBuilder();
+        Node tmp3 = startOfQueue;
+        int count = 0;
+        while(tmp3!=null){
+            tmp.append(tmp3.getPatient().toString());
+            tmp3 = tmp3.getNext();
+            count++;
+        }
+        return "WaitQueue{" +
+                "startOfQueue=" + startOfQueue +
+                ", current queue="+ tmp + "\n"+
+                " counter="+count+
+                '}';
     }
 }
